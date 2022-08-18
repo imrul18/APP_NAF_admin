3b3d8fe5e5460b86e62e481097ead82ba40db718
Imrul Afnan <imrul.afnan18@gmail.com>
2022-08-18 16:29:02 +0600

    version update

--- android/app/src/main/java/com/naf/MainApplication.java
+++ android/app/src/main/java/com/naf/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.safaeean.barcodescanner.BarcodeScannerPackage;
 import com.reactnativecommunity.art.ARTPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.reactnativecommunity.checkbox.ReactCheckBoxPackage;
